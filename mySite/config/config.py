41bfc66724c507b47438d7454e62c6c0a32cd21f
rukawartjxodnd <40817166+rukawartjxodnd@users.noreply.github.com>
2019-08-13 19:51:50 +0900

    .

--- mySite/config/config.py
+++ mySite/config/config.py
@@ -6,4 +6,5 @@ def chromeDriverPath(area="ubuntu"):
         chromeDriverPath = "chromedriver/chromedriver_ubuntu"
     else:
         chromeDriverPath = "chromedriver/chromedriver_ubuntu"
+        chromeDriverPath = ""
     return chromeDriverPath
