6acac528d41153c38bdebcc7318987e0cbf92eec
rukawartjxodnd <40817166+rukawartjxodnd@users.noreply.github.com>
2019-08-13 19:54:36 +0900

    .

--- mySite/stockChoice/crawler/get_magic_formula.py
+++ mySite/stockChoice/crawler/get_magic_formula.py
@@ -104,6 +104,7 @@ def sortingListComplexValue(dataGroupList):
 ###########################################################
 def set_page_driver(sosok):
     driver = webdriver.Chrome(chromeDriverPath)
+    driver = webdriver.Chrome()
     url = "https://finance.naver.com/sise/sise_market_sum.nhn?sosok="+sosok
     driver.get(url)
     click_option1 = driver.find_element_by_xpath("//*[@id='option12']")
